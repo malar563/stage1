8fe0275aa9fbd0c9bdbdc55a9a069420d7728f94
Marylise Larouche <MALAR563@ulaval.ca>
2025-05-08 16:14:33 -0400

    Tentative lecture DICOM

--- Test2.py
+++ Test2.py
@@ -1,3 +1,14 @@
+import pydicom as dicom
+import matplotlib.pyplot as plt
 import numpy as np
+# import scikit
 
-print("allo")
+
+def load_file(file_number=10):
+    return dicom.dcmread(f"DICOM_0{file_number}\COW_Angio_0.6_Hv36_3")
+
+image = load_file()
+
+print(image)
+# plt.imshow(image, cmap='gist_gray')
+# plt.show()
