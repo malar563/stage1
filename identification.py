47c6602158c8bd7ecf4781b7cf07a07f8c3528de
Marylise Larouche <MALAR563@ulaval.ca>
2025-05-20 12:00:53 -0400

    Capable de trouver le nez

--- identification.py
+++ identification.py
@@ -26,14 +26,49 @@ class Identification(Segmentation):
         return self.head
 
 
-    def find_nasion(self, window_y=75):
-        x_half_head = self.head[:,:int(len(self.head[1,:,1])//2),int(len(self.head[1,1,:])//2 - window_y):int(len(self.head[1,1,:])//2 + window_y)]
-        index_z = np.arange(0, len(self.head[:,1,1]), 1)
-        # print(index_z)
+    def find_nasion(self):#75
+        x_half_head = self.head[:,:int(len(self.head[1,:,1])//2),:]
+        
+        # Sum up one values of the binary mask on the x axis (3D array -> 2D array)
         counts_x = np.sum(x_half_head, axis = 1) # axis=2 donne somme en y, axis=0 donne somme en z
-        plt.imshow(counts_x, origin="lower")
+
+        # Index of the maximal values for each axis (new coordinate system)
+        argmax_x = np.argmax(counts_x, axis=0) # En x
+        # argmax_y = np.argmax(counts_x, axis=1) # En y
+
+        # Finding x, y coordinate of the nose
+        from scipy.ndimage import gaussian_filter1d
+        argmax_x_filtered = gaussian_filter1d(argmax_x, sigma=3)
+        deriv = np.gradient(argmax_x_filtered)
+        
+        nose_section = counts_x[:,np.argmin(deriv):np.argmax(deriv)]
+        nose_y = int(np.mean(argmax_x[np.argmin(deriv):np.argmax(deriv)]))
+        # Au lieu de faire une moyenne pour le nez en x, fiter une gaussienne? Mais est-ce que le nez a une forme gaussienne??
+        nose_x = int((np.argmin(deriv)+np.argmax(deriv))//2) - np.argmin(deriv) 
+        
+        plt.imshow(nose_section, origin="lower")
+        plt.scatter([nose_x], [nose_y], c="r")
         plt.show()
-        print(counts_x)
+                
+        print(nose_section[:,nose_x])
+        diff = np.gradient(nose_section, axis=0) # axis=1 donne différentielle du sens transverse au nez
+
+        diff = []
+        for i in range(1, len(nose_section[:,nose_x])):
+            diff.append(nose_section[i,nose_x]-nose_section[i-1,nose_x])
+        # for i in range(1, len(nose_section[:,0])):
+        #     diff.append(nose_section[i,0]-nose_section[i-1,0])
+        print(diff)
+
+        # plt.imshow(diff, origin="lower")
+        # plt.show()
+        # plt.plot(diff[:,nose_x])
+        # plt.plot(gaussian_filter1d(diff, sigma=1))
+        plt.plot(diff)
+        plt.show()
+
+
+
         self.head = x_half_head
         return self.head
     
