42846feb52ac63615df2fe09356ede0a5f7a50f9
Marylise Larouche <MALAR563@ulaval.ca>
2025-07-04 14:13:12 -0400

    Écrire si pipeline pas marché dans fichier texte

--- main.py
+++ main.py
@@ -1159,41 +1159,45 @@ dicoms_list = ["ct_enligne/6", "ct_enligne/7"]
 # CHECKER LES AXES PARTOUT POUR ÊTRE SÛR QUE C'EST CHILL
 def main(dicoms_list = dicoms_list):
     for i, dicom in enumerate(dicoms_list):
-        start = time.time()
-        ct = Segmentation(dcm_path=dicom, big_output_directory="ct_enligne_nifti", file_number=i+4)
-        ct.apply_threshold()       
-        ct.keep_largest_island()
-        ct.fill_holes()
-        ct.remove_arteries()
+        try:
+            start = time.time()
+            ct = Segmentation(dcm_path=dicom, big_output_directory="ct_enligne_nifti", file_number=i+4)
+            ct.apply_threshold()       
+            ct.keep_largest_island()
+            ct.fill_holes()
+            ct.remove_arteries()
 
-        # Totalsegmentator
-        ct.segment_brain()
-        ct.arteries_and_totalsegmentator_mask()
-        ct.mask_to_nii()
+            # Totalsegmentator
+            ct.segment_brain()
+            ct.arteries_and_totalsegmentator_mask()
+            ct.mask_to_nii()
 
-        # ct.show_3D_array(ct.skull, axis=2) # En z
-        # ct.show_3D_array(ct.head, axis=0, pt=(50,42), pt_slice = 100)
-        id = Registration(big_output_directory="ct_enligne_nifti", file_number=i+4, fixed_img_path='icbm_avg_152_t1_tal_lin.nii')
+            # ct.show_3D_array(ct.skull, axis=2) # En z
+            # ct.show_3D_array(ct.head, axis=0, pt=(50,42), pt_slice = 100)
+            id = Registration(big_output_directory="ct_enligne_nifti", file_number=i+4, fixed_img_path='icbm_avg_152_t1_tal_lin.nii')
 
-        id.register(show=True)
-        # id.read_transforms()
+            id.register(show=True)
+            # id.read_transforms()
 
-        id.find_registered_lpa_rpa_nasion()
-        id.fill_cavities()
-        id.find_nasion()
-        id.check_nasion()
-        id.find_rpa()
-        print("rpa :", id.rpa)
-        print("registered_rpa :", id.registered_rpa)
-        id.show_3D_array(id.head, axis=2, pt=(id.rpa[1], id.rpa[0]), pt_slice=id.rpa[2])
-        id.show_3D_array(id.head, axis=2, pt=(id.registered_rpa[1], id.registered_rpa[0]), pt_slice=id.registered_rpa[2])
-        id.find_lpa()
-        print("lpa :", id.lpa)
-        print("registered_rpa :", id.registered_lpa)
-        id.show_3D_array(id.head, axis=2, pt=(id.lpa[1], id.lpa[0]), pt_slice=id.lpa[2])
-        id.show_3D_array(id.head, axis=2, pt=(id.registered_lpa[1], id.registered_lpa[0]), pt_slice=id.registered_lpa[2])
+            id.find_registered_lpa_rpa_nasion()
+            id.fill_cavities()
+            id.find_nasion()
+            id.check_nasion()
+            id.find_rpa()
+            print("rpa :", id.rpa)
+            print("registered_rpa :", id.registered_rpa)
+            id.show_3D_array(id.head, axis=2, pt=(id.rpa[1], id.rpa[0]), pt_slice=id.rpa[2])
+            id.show_3D_array(id.head, axis=2, pt=(id.registered_rpa[1], id.registered_rpa[0]), pt_slice=id.registered_rpa[2])
+            id.find_lpa()
+            print("lpa :", id.lpa)
+            print("registered_rpa :", id.registered_lpa)
+            id.show_3D_array(id.head, axis=2, pt=(id.lpa[1], id.lpa[0]), pt_slice=id.lpa[2])
+            id.show_3D_array(id.head, axis=2, pt=(id.registered_lpa[1], id.registered_lpa[0]), pt_slice=id.registered_lpa[2])
 
-        print(f"Time to segment file {ct.nii_path} : {time.time() - start} seconds")
+            print(f"Time to segment file {ct.nii_path} : {time.time() - start} seconds")
+        except Exception as e:
+            with open(os.path.join(ct.big_output_directory, 'error.txt'), 'a') as file:
+                file.write(f"{ct.nii_path} : {e}\n")
 
     # start = time.time()
     # ct = Segmentation(dcm_path="online_patient/2.16.840.1.114274.1818.56920369040074765021783555636978216368", big_output_directory="online", file_number=2)
