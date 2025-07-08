5d06635863e15fb2898393dd89524f7e3f54f91d
Marylise Larouche <MALAR563@ulaval.ca>
2025-07-08 16:20:08 -0400

    Afficher marche avec plusieurs fichiers!!

--- main.py
+++ main.py
@@ -16,68 +16,65 @@ def run_everything(dicoms_list, big_output_directory="cava", verbose=True):
 
             ct.apply_threshold()
             if verbose:
-                print("Thresholds were applied")
+                print("Thresholds applied")
 
             ct.keep_largest_island()
             if verbose:
-                print("Largest island was kept")
+                print("Kept largest island")
 
             ct.show_3D_array(ct.skull, axis=2) 
             ct.fill_holes()
             if verbose:
-                print("Holes were filled")
+                print("Holes filled")
 
             ct.remove_arteries()
             if verbose:
-                print("Arteries at distance were removed")
-
+                print("Distance arteries removed")
             # Totalsegmentator
             ct.segment_brain()
             if verbose:
-                print("Head was segmented with TotalSegmentator")
+                print("Head segmented (TotalSegmentator)")
 
             ct.arteries_and_totalsegmentator_mask()
             if verbose:
-                print("Masks derived from TotalSegmentator were created")
+                print("TotalSegmentator masks created")
 
             ct.mask_to_nii()
             if verbose:
-                print(f"Segmentation is done for {ct.nii_path}")
-
+                print(f"Segmentation complete: {ct.nii_path}")
 
             # ct.show_3D_array(ct.skull, axis=2) # En z
             # ct.show_3D_array(ct.head, axis=0, pt=(50,42), pt_slice = 100)
             id = Identification(big_output_directory=big_output_directory, file_number=i, fixed_img_path='icbm_avg_152_t1_tal_lin.nii')
 
             # id.register(show=True)
-            
+            # if verbose:
+            #     print("Registration done")
+
             id.read_transforms()
             if verbose:
-                print("Transforms were read")
-               
+                print("Transforms loaded")
+
             id.find_registered_lpa_rpa_nasion()
             if verbose:
-                print("Nasion, LPA and RPA were found with registration")
+                print("Registered LPA, RPA, and Nasion located")
 
             id.find_nasion()
             if verbose:
-                print("Nasion's position was refined")
+                print("Nasion refined")
 
-            id.find_depth_rpa()
-            id.find_depth_lpa()
+            id.improve_lpa_rpa()
             if verbose:
-                print("RPA's and LPA's position were refined")
+                print("LPA and RPA refined")
 
             id.check_nasion()
             print("rpa :", id.rpa)
             print("registered_rpa :", id.registered_rpa)
-            id.show_3D_array(id.head, axis=2, pt=(id.rpa[1], id.rpa[0]), pt_slice=id.rpa[2])
-            id.show_3D_array(id.head, axis=2, pt=(id.registered_rpa[1], id.registered_rpa[0]), pt_slice=id.registered_rpa[2])
+            id.show_3D_array(id.head, axis=2, pts=[((id.rpa[1], id.rpa[0]),id.rpa[2], "red"), ((id.registered_rpa[1], id.registered_rpa[0]),id.registered_rpa[2], "green")])
             
             print("lpa :", id.lpa)
             print("registered_rpa :", id.registered_lpa)
-            id.show_3D_array(id.head, axis=2, pt=(id.lpa[1], id.lpa[0]), pt_slice=id.lpa[2])
-            id.show_3D_array(id.head, axis=2, pt=(id.registered_lpa[1], id.registered_lpa[0]), pt_slice=id.registered_lpa[2])
+            id.show_3D_array(id.head, axis=2, pts=[((id.lpa[1], id.lpa[0]),id.lpa[2], "red"), ((id.registered_lpa[1], id.registered_lpa[0]),id.registered_lpa[2], "green")])
 
             print(f"Time to segment file {ct.nii_path} : {time.time() - start} seconds")
 
@@ -88,7 +85,7 @@ def run_everything(dicoms_list, big_output_directory="cava", verbose=True):
 
 if __name__ == "__main__":
 
-    from automatically_get_dicom_folders import get_dicom_folders
+    from automatically_get_dicom_folders import create_dicom_list
 
     dicoms_list = ["ct_enligne/1", "ct_enligne/2","ct_enligne/4","ct_enligne/5", "ct_enligne/6", "ct_enligne/7"]
     # dicoms_list = create_dicom_list(directory="nifti")
