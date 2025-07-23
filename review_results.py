01ea069173055e923825d520af247dac01a5100f
Marylise Larouche <MALAR563@ulaval.ca>
2025-07-23 09:34:07 -0400

    Afficher dans view_results marche (à améliorer)

--- review_results.py
+++ review_results.py
@@ -1,86 +1,68 @@
 import os
-import time
 import nibabel as nib
-import numpy as np
 import pandas as pd
-
-
-
-
-
-
 from class_identification import Identification
-# for i, nifti in enumerate(dicoms_list):
-
-#     id = Registration(big_output_directory="online", file_number=i, fixed_img_path='icbm_avg_152_t1_tal_lin.nii')
-
-#     # id.register(show=True)
-#     id.read_transforms()
-
-#     id.find_registered_lpa_rpa_nasion()
-#     id.fill_cavities()
-#     id.find_nasion()
-#     id.check_nasion()
-#     id.find_rpa()
-#     print("rpa :", id.rpa)
-#     id.show_3D_array(id.head, axis=2, pt=(id.rpa[1], id.rpa[0]), pt_slice=id.rpa[2])
-#     id.find_lpa()
-#     print("lpa :", id.lpa)
-#     id.show_3D_array(id.head, axis=2, pt=(id.lpa[1], id.lpa[0]), pt_slice=id.lpa[2])
-
-#     # id.save_pts_to_csv()
-#     # id.mca_territory_mask()
 
 
-
-
-# AVANT CETAIT PAS EN COMMENTAIRE ->
-
-# # 6_cta_thins # online_angio no.2
-id = Identification(big_output_directory="50_2025-07-17", file_number=8, fixed_img_path='icbm_avg_152_t1_tal_lin.nii')#50_p_2025-07-15
+# To check the registration and the head
+id = Identification(big_output_directory="cava", file_number=1, fixed_img_path='icbm_avg_152_t1_tal_lin.nii')
 id.show_3D_array(id.head, axis=0)
 
 
-# # id.register(show=True)
-# id.read_transforms()
+# def load_landmarks_from_csv(csv_path):
+#     """Load and separate MRI and CT landmarks from a CSV file."""
+#     df = pd.read_csv(csv_path, sep=",")
+    
+#     # Get MRI landmarks
+#     array_MRI = df.iloc[6:12, 1:].values.astype(float)
+#     reg_nas_MRI, reg_lpa_MRI, reg_rpa_MRI = array_MRI[1], array_MRI[3], array_MRI[5]
+#     imp_nas_MRI, imp_lpa_MRI, imp_rpa_MRI = array_MRI[0], array_MRI[2], array_MRI[4]
+    
+#     # Get CT landmarks
+#     array_CT = df.iloc[13:19, 1:].values.astype(float)
+#     reg_nas_CT, reg_lpa_CT, reg_rpa_CT = array_CT[1], array_CT[3], array_CT[5]
+#     imp_nas_CT, imp_lpa_CT, imp_rpa_CT = array_CT[0], array_CT[2], array_CT[4]
 
-# id.find_registered_lpa_rpa_nasion()
-# id.fill_cavities()
-# id.find_nasion(window=5)
-# # id.check_nasion()
-# id.improve_lpa_rpa()
-# print("rpa :", id.rpa)
-# print("registered_rpa :", id.registered_rpa)
-# id.show_3D_array(id.head, axis=2, pts=[((id.rpa[1], id.rpa[0]),id.rpa[2], "red"), ((id.registered_rpa[1], id.registered_rpa[0]),id.registered_rpa[2], "green")])
-# # id.show_3D_array(id.head, axis=2, pt=(id.registered_rpa[1], id.registered_rpa[0]), pt_slice=id.registered_rpa[2])
-# print("lpa :", id.lpa)
-# print("registered_rpa :", id.registered_lpa)
-# id.show_3D_array(id.head, axis=2, pts=[((id.lpa[1], id.lpa[0]),id.lpa[2], "red"), ((id.registered_lpa[1], id.registered_lpa[0]),id.registered_lpa[2], "green")])
-# # id.show_3D_array(id.head, axis=2, pt=(id.lpa[1], id.lpa[0]), pt_slice=id.lpa[2])
-# # id.show_3D_array(id.head, axis=2, pt=(id.registered_lpa[1], id.registered_lpa[0]), pt_slice=id.registered_lpa[2])
-
-# # id.delete_useless_files()
-# id.save_pts_to_csv()
-# # # id.mca_territory_mask()
+#     return {"reg": {"MRI": [reg_nas_MRI, reg_lpa_MRI, reg_rpa_MRI],
+#                     "CT":  [reg_nas_CT,  reg_lpa_CT,  reg_rpa_CT]},
+#             "imp": {"MRI": [imp_nas_MRI, imp_lpa_MRI, imp_rpa_MRI],
+#                     "CT":  [imp_nas_CT,  imp_lpa_CT,  imp_rpa_CT]}}
 
 
 
 
-
-
-
-# Check if the registration is fine
 csv_path = os.path.join(id.nifti_output_directory, "points"+id.file_number+".csv")
 df = pd.read_csv(csv_path, sep=",")
 
 array_MRI = df.iloc[6:12,1:].values.astype(float)
+reg_nas_MRI, reg_lpa_MRI, reg_rpa_MRI = array_MRI[1], array_MRI[3], array_MRI[5]
+imp_nas_MRI, imp_lpa_MRI, imp_rpa_MRI = array_MRI[0], array_MRI[2], array_MRI[4]
 print(array_MRI)
-reg_nas_MRI = array_MRI[1]
-reg_lpa_MRI = array_MRI[3]
-reg_rpa_MRI = array_MRI[5]
-print(reg_nas_MRI, reg_lpa_MRI, reg_rpa_MRI)
 
-img = nib.load(id.moving_img_path)
+array_CT = df.iloc[13:19,1:].values.astype(float)
+reg_nas_CT, reg_lpa_CT, reg_rpa_CT = array_CT[1], array_CT[3], array_CT[5]
+imp_nas_CT, imp_lpa_CT, imp_rpa_CT = array_CT[0], array_CT[2], array_CT[4]
+print(array_CT)
+
+head = nib.load(id.moving_img_path)
+head = head.get_fdata()
+
+# OK to change lpa, rpa nas by other values depending on what you want to see
+lpa = reg_lpa_CT
+rpa = reg_rpa_CT
+nas = reg_nas_CT
+id.show_3D_array(head, axis=0, pts=[ ((lpa[2],lpa[0]),lpa[1],"blue"),  ((rpa[2],rpa[0]),rpa[1],"red"), ((nas[2],nas[0]),nas[1],"green")])
+id.show_3D_array(head, axis=1, pts=[ ((lpa[2],lpa[1]),lpa[0],"blue"),  ((rpa[2],rpa[1]),rpa[0],"red"), ((nas[2],nas[1]),nas[0],"green")])
+
+# Comment the name to keep what you want to see
+nifti_img = "totalsegmentator"
+nifti_img = "mask"
+nifti_img = "mca_territory"
+img = nib.load(os.path.join(id.nifti_output_directory, nifti_img+id.file_number+".nii.gz"))
+
+nifti_img = "6_cow_angio__06__hv36__3"
+img = nib.load(os.path.join(id.nifti_output_directory, nifti_img+".nii.gz"))
+
 img = img.get_fdata()
-id.show_3D_array(img, axis=0, pts=[ ((reg_lpa_MRI[2],reg_lpa_MRI[0]),reg_lpa_MRI[1],"blue"),  ((reg_rpa_MRI[2],reg_rpa_MRI[0]),reg_rpa_MRI[1],"red")])
-id.show_3D_array(img, axis=1, pts=[ ((reg_nas_MRI[2],reg_nas_MRI[1]),reg_nas_MRI[0],"red") ])
+id.show_3D_array(img, axis=2)
+
